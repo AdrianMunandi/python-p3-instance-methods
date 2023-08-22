8960a34a4c396f8874478965e1cd2698d7a6971a
AdrianMunandi <adrian@ex.example.com>
2023-08-22 11:08:51 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-
+# dog.py
 class Dog:
-    # Class body goes here
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating an instance of the Dog class
+my_dog = Dog("Buddy", "Golden Retriever")
+
+# Calling the methods
+my_dog.bark()
+my_dog.sit()
--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -1,49 +1,46 @@
 #!/usr/bin/env python3
 
+import unittest
 from dog import Dog
-
 import io
 import sys
 import types
 
-class TestDog:
-    '''Dog in dog.py'''
-
+class TestDog(unittest.TestCase):
     def test_is_class(self):
-        '''is a class with the name "Dog"'''
-        fido = Dog()
-        assert(type(fido) == Dog)
+        '''Dog in dog.py is a class with the name "Dog"'''
+        fido = Dog("Buddy", "Golden Retriever")
+        self.assertTrue(isinstance(fido, Dog))
 
-class TestBark:
-    '''Dog.bark() in dog.py'''
-
+class TestBark(unittest.TestCase):
     def test_is_method(self):
-        '''is an instance method'''
-        fido = Dog()
-        assert(type(fido.bark) == types.MethodType)
+        '''Dog.bark() in dog.py is an instance method'''
+        fido = Dog("Buddy", "Golden Retriever")
+        self.assertTrue(isinstance(fido.bark, types.MethodType))
 
     def test_prints_woof(self):
-        '''prints "Woof!"'''
-        fido = Dog()
+        '''Dog.bark() in dog.py prints "Woof!"'''
+        fido = Dog("Buddy", "Golden Retriever")
         captured_out = io.StringIO()
         sys.stdout = captured_out
         fido.bark()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Woof!\n")
+        self.assertEqual(captured_out.getvalue(), "Woof!\n")
 
-class TestSit:
-    '''Dog.sit() in dog.py'''
-
+class TestSit(unittest.TestCase):
     def test_is_method(self):
-        '''is an instance method'''
-        fido = Dog()
-        assert(type(fido.sit) == types.MethodType)
+        '''Dog.sit() in dog.py is an instance method'''
+        fido = Dog("Buddy", "Golden Retriever")
+        self.assertTrue(isinstance(fido.sit, types.MethodType))
 
     def test_prints_the_dog_is_sitting(self):
-        '''prints "The dog is sitting."'''
-        fido = Dog()
+        '''Dog.sit() in dog.py prints "The dog is sitting."'''
+        fido = Dog("Buddy", "Golden Retriever")
         captured_out = io.StringIO()
         sys.stdout = captured_out
         fido.sit()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "The dog is sitting.\n")
+        self.assertEqual(captured_out.getvalue(), "The dog is sitting.\n")
+
+if __name__ == '__main__':
+    unittest.main()
